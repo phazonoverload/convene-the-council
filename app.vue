f3336a4a608bef6a20c5ca18d157cbef04529ebd
phazonoverload <kvn@lws.io>
2026-04-14 14:56:40 +0200

    feat: add main app layout with header

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-parchment">
+    <AppHeader />
+    <main class="max-w-6xl mx-auto px-4 py-8">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<style>
+@import '~/assets/css/main.css';
+</style>
